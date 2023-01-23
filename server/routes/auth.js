c932a61c1a36f51d686ab9d6c52d533b7f0d96af
ubaidubai <avez3npqureshi@gmail.com>
2023-01-23 14:51:27 +0530

    Till Step 9

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,6 +7,8 @@ router.get('/', (req, res) => {
     res.send("Hello world from the server to the Home")
 })
 
+/*
+
 // Step 8 using Promises to store inside the online database
 router.post('/register', (req, res) => {
     
@@ -35,6 +37,43 @@ router.post('/register', (req, res) => {
                 res.status(201).json({message:"User registered Successfully"});
             }).catch((err)=>{console.log(err);});
         })
+}) 
+
+*/
+
+
+//Step 9 : using Async Await to store inside the online database 
+
+router.post('/register', async (req, res) => {
+    //No.1 getting the data which user has filled in the form
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    //No.2 Check whether all fields are entered properly in the form
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please enter the data , properly in all fields" })
+    }
+
+    try {
+        //No.3 Checking whether user is unique by matching email present in the database with the email given by the user
+        const userExists = await User.findOne({ email: email });
+
+        //if email is duplicate then show the below error
+        if (userExists) {
+            return res.status(422).json({ error: "Email already Exist" });
+        }
+
+        //if email dosent exists then add the user into database 
+        //Step 1 : fill all the data from the model in the variable user 
+        const user = new User({ name, email, phone, work, password, cpassword });
+
+        //Step 2 : save the variable user using save method 
+        await user.save();
+        res.status(201).json({ message: "User registered successfully" })
+    } catch (err) {
+        console.log(err);
+    }
+
+
 })
 
 module.exports = router
